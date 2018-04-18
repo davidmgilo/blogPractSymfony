7a9b1f0a41bbd75809b2dc7353e6cf47544634cb
David Martínez Gilo <davidmgilo@gmail.com>
2018-04-18 19:35:15 +0200

    list

--- src/BlogBundle/Controller/DefaultController.php
+++ src/BlogBundle/Controller/DefaultController.php
@@ -8,6 +8,19 @@ class DefaultController extends Controller
 {
     public function indexAction()
     {
-        return $this->render('BlogBundle:Default:index.html.twig');
+        $em = $this->getDoctrine()->getEntityManager();
+        $entry_repo = $em->getRepository("BlogBundle:Entry");
+        $entries = $entry_repo->findAll();
+        
+        foreach($entries as $entry){
+            echo $entry->getTitle().'<br/>';
+            echo $entry->getCategory()->getName().'<br/>';
+            echo $entry->getUser()->getName().'<br/>';
+            
+            echo "<hr/>";
+        }
+        
+        die();
+//        return $this->render('BlogBundle:Default:index.html.twig');
     }
 }
