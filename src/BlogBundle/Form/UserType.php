ce736c9da54d46ba36b622e725e27e94f08ce5b2
David Martínez Gilo <davidmgilo@gmail.com>
2018-04-25 19:55:56 +0200

    login part V - php bin/console doctrine:generate:form BlogBundle:User

---
+++ src/BlogBundle/Form/UserType.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace BlogBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class UserType extends AbstractType
+{
+    /**
+     * @param FormBuilderInterface $builder
+     * @param array $options
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('role')
+            ->add('name')
+            ->add('surname')
+            ->add('email')
+            ->add('password')
+            ->add('imagen')
+        ;
+    }
+    
+    /**
+     * @param OptionsResolver $resolver
+     */
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'BlogBundle\Entity\User'
+        ));
+    }
+}
