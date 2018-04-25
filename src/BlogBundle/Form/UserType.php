6274aeb5f40e0b8ac93ac50c04129c28db86be59
David Martínez Gilo <davidmgilo@gmail.com>
2018-04-25 20:30:11 +0200

    register part II

--- src/BlogBundle/Form/UserType.php
+++ src/BlogBundle/Form/UserType.php
@@ -5,6 +5,10 @@ namespace BlogBundle\Form;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 
 class UserType extends AbstractType
 {
@@ -15,10 +19,21 @@ class UserType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('name')
-            ->add('surname')
-            ->add('email')
-            ->add('password')
+             ->add('name',TextType::class, array("label"=>"Nom","required"=>"required", "attr" =>array(
+				"class" => "form-name form-control",
+			)))
+            ->add('surname',TextType::class, array("label"=>"Cognom","required"=>"required", "attr" =>array(
+				"class" => "form-surname form-control",
+			)))
+            ->add('email',EmailType::class, array("label"=>"Email","required"=>"required", "attr" =>array(
+				"class" => "form-email form-control",
+			)))
+            ->add('password',PasswordType::class, array("label"=>"Password","required"=>"required", "attr" =>array(
+				"class" => "form-password form-control",
+			)))
+			->add('Guardar', SubmitType::class, array("attr" =>array(
+				"class" => "form-submit btn btn-success",
+			)))
         ;
     }
     
