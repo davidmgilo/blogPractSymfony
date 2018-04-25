4020ccfc68d00537ae73a41953d90d5ae108c35e
David Martínez Gilo <davidmgilo@gmail.com>
2018-04-25 18:10:37 +0200

    layout

--- src/BlogBundle/Controller/DefaultController.php
+++ src/BlogBundle/Controller/DefaultController.php
@@ -8,24 +8,24 @@ class DefaultController extends Controller
 {
     public function indexAction()
     {
-        $em = $this->getDoctrine()->getEntityManager();
-        $entry_repo = $em->getRepository("BlogBundle:Entry");
-        $entries = $entry_repo->findAll();
-        
-        foreach($entries as $entry){
-            echo $entry->getTitle().'<br/>';
-            echo $entry->getCategory()->getName().'<br/>';
-            echo $entry->getUser()->getName().'<br/>';
-            $tags = $entry->getEntryTag();
-            foreach ($tags as $tag){
-                echo $tag->getTag()->getName().'<br/>';
-            }
-            
-            echo "<hr/>";
-        }
-        
-        die();
-//        return $this->render('BlogBundle:Default:index.html.twig');
+//        $em = $this->getDoctrine()->getEntityManager();
+//        $entry_repo = $em->getRepository("BlogBundle:Entry");
+//        $entries = $entry_repo->findAll();
+//        
+//        foreach($entries as $entry){
+//            echo $entry->getTitle().'<br/>';
+//            echo $entry->getCategory()->getName().'<br/>';
+//            echo $entry->getUser()->getName().'<br/>';
+//            $tags = $entry->getEntryTag();
+//            foreach ($tags as $tag){
+//                echo $tag->getTag()->getName().'<br/>';
+//            }
+//            
+//            echo "<hr/>";
+//        }
+//        
+//        die();
+        return $this->render('BlogBundle:Default:index.html.twig');
     }
     
     public function categoriesAction()
