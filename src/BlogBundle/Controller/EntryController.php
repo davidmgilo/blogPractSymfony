56b0ca4e8b23d97013de53968dd2387d0bdd8431
David Martínez Gilo <davidmgilo@gmail.com>
2018-05-02 19:48:46 +0200

    categories from entry & delete entry

--- src/BlogBundle/Controller/EntryController.php
+++ src/BlogBundle/Controller/EntryController.php
@@ -88,12 +88,38 @@ class EntryController extends Controller
 		$em = $this->getDoctrine()->getEntityManager();
 		$entry_repo=$em->getRepository("BlogBundle:Entry");
                 $entries=$entry_repo->findAll();
-               
-		
+                $category_repo=$em->getRepository("BlogBundle:Category");
+		$categories=$category_repo->findAll();
+                
                 return $this->render("BlogBundle:Entry:index.html.twig",array(
 			"entries" => $entries,
+                        "categories" => $categories,
 
 		));
-	}    
+	} 
+        
+    public function deleteAction($id){
+            $em = $this->getDoctrine()->getEntityManager();
+            $entry_repo=$em->getRepository("BlogBundle:Entry");
+            $entry_tag_repo=$em->getRepository("BlogBundle:EntryTag");
+
+            $entry=$entry_repo->find($id);
+
+            $entry_tags=$entry_tag_repo->findBy(array("entry"=>$entry));
+
+            foreach($entry_tags as $et){
+                    if(is_object($et)){
+                            $em->remove($et);
+                            $em->flush();
+                    }
+            }
+
+            if(is_object($entry)){
+                    $em->remove($entry);
+                    $em->flush();
+            }
+
+            return $this->redirectToRoute("blog_homepage");
+    }
 	
 }
