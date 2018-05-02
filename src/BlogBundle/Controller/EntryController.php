2805ccac25650998c9818d7f338d0944c3bc4043
David Martínez Gilo <davidmgilo@gmail.com>
2018-05-02 20:20:20 +0200

    edit entry

--- src/BlogBundle/Controller/EntryController.php
+++ src/BlogBundle/Controller/EntryController.php
@@ -122,4 +122,90 @@ class EntryController extends Controller
             return $this->redirectToRoute("blog_homepage");
     }
 	
+    public function editAction(Request $request, $id){
+		$em = $this->getDoctrine()->getEntityManager();
+		$entry_repo = $em->getRepository("BlogBundle:Entry");
+		$category_repo = $em->getRepository("BlogBundle:Category");
+		
+		$entry=$entry_repo->find($id);
+		$entry_image=$entry->getImage();
+		
+		$tags="";
+		foreach($entry->getEntryTag() as $entryTag){
+			$tags.=$entryTag->getTag()->getName().",";
+		}
+		
+		$form = $this->createForm(EntryType::class, $entry);
+		
+		$form->handleRequest($request);
+		
+		if($form->isSubmitted()){
+			if($form->isValid()){
+				
+				/*
+					$entry->setTitle($form->get("title")->getData());
+					$entry->setContent($form->get("content")->getData());
+					$entry->setStatus($form->get("status")->getData());
+				 */
+				
+				// upload file
+				$file=$form["image"]->getData();
+				
+				if(!empty($file) && $file!=null){
+					$ext=$file->guessExtension();
+					$file_name=time().".".$ext;
+					$file->move("uploads",$file_name);
+
+					$entry->setImage($file_name);
+				}else{
+					$entry->setImage($entry_image);
+				}
+
+				$category = $category_repo->find($form->get("category")->getData());
+				$entry->setCategory($category);
+				
+				$user=$this->getUser();
+				$entry->setUser($user);
+				
+				$em->persist($entry);
+				$flush=$em->flush();
+				
+				
+				$entry_tag_repo=$em->getRepository("BlogBundle:EntryTag");
+				$entry_tags=$entry_tag_repo->findBy(array("entry"=>$entry));
+
+				foreach($entry_tags as $et){
+					if(is_object($et)){
+						$em->remove($et);
+						$em->flush();
+					}
+				}
+				
+				$entry_repo->saveEntryTags(
+						$form->get("tags")->getData(),
+						$form->get("title")->getData(),
+						$category,
+						$user
+						);
+				
+				if($flush==null){
+					$status = "Entrada creada correctament";
+				}else{
+					$status = "Alguna cosa ha fallat";
+				}
+				
+			}else{
+				$status = "Formulari no vàlid";
+			}
+			
+			$this->session->getFlashBag()->add("status", $status);
+			return $this->redirectToRoute("blog_homepage");
+		}
+		
+		return $this->render("BlogBundle:Entry:edit.html.twig",array(
+			"form" => $form->createView(),
+			"entry" => $entry,
+			"tags" => $tags
+		));
+	}
 }
